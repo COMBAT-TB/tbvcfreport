bb7e94ddd266c2eb1c7d323eac2fd9bf27f9b1e9
thobalose <lose.thoba@gmail.com>
2018-03-23 14:24:07 +0200

    Update package and repo name.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='tbvcfreport',
     version='0.0.1',
-    url='https://github.com/COMBAT-TB/tb-vcf-report',
-    bugtrack_url='https://github.com/COMBAT-TB/tb-vcf-report/issues',
+    url='https://github.com/COMBAT-TB/tbvcfreport',
+    bugtrack_url='https://github.com/COMBAT-TB/tbvcfreport/issues',
     description='Parses SnpEff generated VCF file and creates an HTML based report with links to Combat TB Explorer.',
     keywords='neo4j,vcf,tb',
     py_modules=['tbvcfreport'],
