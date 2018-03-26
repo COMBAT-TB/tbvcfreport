2cfc6b4b8205aa28ce82a603ed1f43618bcbd2d4
Peter van Heusden <pvh@sanbi.ac.za>
2018-03-26 15:54:40 +0200

    Added package_data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbvcfreport',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/COMBAT-TB/tbvcfreport',
     bugtrack_url='https://github.com/COMBAT-TB/tbvcfreport/issues',
     description='Parses SnpEff generated VCF file and creates an HTML based report with links to Combat TB Explorer.',
@@ -10,6 +10,9 @@ setup(
     py_modules=['tbvcfreport'],
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        'tbvcfreport': ['templates/*.html'],
+    },
     install_requires=[
         'click',
         'py2neo',
