b255e72c131ca0a83327b530f5b10fbd888f8cce
thobalose <lose.thoba@gmail.com>
2018-03-28 10:04:55 +0200

    Added MIT license.

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     bugtrack_url='https://github.com/COMBAT-TB/tbvcfreport/issues',
     description='Parses SnpEff generated VCF file and creates an HTML based report with links to Combat TB Explorer.',
     keywords='neo4j,vcf,tb',
+    license="MIT",
     py_modules=['tbvcfreport'],
     packages=find_packages(),
-    include_package_data=True,
     package_data={
         'tbvcfreport': ['templates/*.html'],
     },
