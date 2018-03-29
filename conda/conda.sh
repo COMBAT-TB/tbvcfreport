5bc3da89bd1f4e01f74f045e42f2955273dc884b
thobalose <lose.thoba@gmail.com>
2018-03-29 11:29:32 +0200

    Added conda-build

--- conda/conda.sh
+++ conda/conda.sh
@@ -5,7 +5,7 @@ USER=sanbi-sa
 OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 wget https://github.com/COMBAT-TB/tbvcfreport/archive/0.0.3.tar.gz
-export SHA = $(sha256sum 0.0.3.tar.gz | awk '{print $1}')
+export SHA=$(sha256sum 0.0.3.tar.gz | awk '{print $1}')
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=0.0.3
