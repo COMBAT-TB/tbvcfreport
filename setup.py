9bc14c8d43f59747756fc072303827ac2e26103e
thoba <lose.thoba@gmail.com>
2019-08-08 14:22:15 +0200

    Bumped version and Galaxy tool xml.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tbvcfreport',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/COMBAT-TB/tbvcfreport',
     bugtrack_url='https://github.com/COMBAT-TB/tbvcfreport/issues',
     description="Parses SnpEff generated VCF and generates an HTML report.",
