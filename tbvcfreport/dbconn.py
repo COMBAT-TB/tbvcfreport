c09023b7fdce90ec96b60623c80a9544513fed54
Peter van Heusden <pvh@sanbi.ac.za>
2021-07-04 11:59:36 +0200

    Switch to Neo4j python driver, support TB Profiler 3, drop SNPIT

--- tbvcfreport/dbconn.py
+++ tbvcfreport/dbconn.py
@@ -1,58 +1,41 @@
 """Interface to Graph Database."""
-import os
-
-from py2neo import Graph
-
-DB_HOST = os.environ.get("DATABASE_URI", "neodb.sanbi.ac.za")
-SECURE = True if DB_HOST == "neodb.sanbi.ac.za" else os.environ.get(
-    "SECURE", False)
-
-graph = Graph(host=DB_HOST, password="", secure=SECURE)
+from neo4j import GraphDatabase
 
 
-def get_gene_data(q):
-    """Query DB.
+class CombatTbDb:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    :param q:
-    :return:
-    """
-    where_statement = "WHERE gene.uniquename='{0}'".format(q)
-    try:
-        data = graph.run(
-            "MATCH (gene:Gene) {where_statement} "
-            "OPTIONAL MATCH (protein:Protein)<-[:ENCODES]-(gene) "
+    def close(self):
+        self.driver.close()
+
+    @staticmethod
+    def _count_genes_tx(tx):
+        result = tx.run("MATCH (g:Gene) return count(g)")
+        return result.single()[0]
+
+    def count_genes(self):
+        with self.driver.session() as session:
+            count = session.read_transaction(self._count_genes_tx)
+        return count
+
+    @staticmethod
+    def _get_gene_table_tx(tx, genes=None):
+        if not genes:
+            where_clause = ""
+        else:
+            where_clause = "WHERE gene.uniquename IN {} ".format(genes)
+        query = (
+            "MATCH (gene:Gene) {} OPTIONAL MATCH (protein:Protein)<-[:ENCODES]-(gene) "
             "OPTIONAL MATCH (pathway:Pathway)<-[:INVOLVED_IN]-(protein) "
             "RETURN gene, protein, "
             "collect(distinct(pathway)) as pathway "
             "order by gene.uniquename asc"
-            .format(where_statement=where_statement)).data()
-        if data:
-            return data[0]
-        else:
-            return {}
-    except Exception as e:
-        raise e
+        ).format(where_clause)
+        result = tx.run(query)
+        return result.values()
 
-
-def query_by_gene_list(genes):
-    """Query DB.
-
-    :param genes: list
-    :return:
-    """
-    where_statement = "WHERE gene.uniquename IN {0}".format(genes)
-    try:
-        data = graph.run(
-            "MATCH (gene:Gene) {where_statement} "
-            "OPTIONAL MATCH (protein:Protein)<-[:ENCODES]-(gene) "
-            "OPTIONAL MATCH (pathway:Pathway)<-[:INVOLVED_IN]-(protein) "
-            "RETURN gene, protein, "
-            "collect(distinct(pathway)) as pathway "
-            "order by gene.uniquename asc"
-            .format(where_statement=where_statement)).data()
-        if data:
-            return data
-        else:
-            return {}
-    except Exception as e:
-        raise e
+    def query_by_gene_list(self, gene_list):
+        with self.driver.session() as session:
+            values = session.read_transaction(self._get_gene_table_tx, gene_list)
+        return values
